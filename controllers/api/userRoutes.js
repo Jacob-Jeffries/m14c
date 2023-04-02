0bfc93c3a90b5417a9c09e1d82e64bb3ff2aef01
Jacob Jeffries <jeffrij85@gmail.com>
2023-04-02 18:12:46 -0400

    Finsied CRUD routes for users.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // Create New User
-router.post('/createUser', async (req, res) => {
+router.post('/create', async (req, res) => {
   try {
+    console.log(`\nCreating new user: ${req.body.username}.`);
     const userData = await User.create({
       first_name: req.body.first_name,
       last_name: req.body.last_name,
@@ -20,6 +21,7 @@ router.post('/createUser', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
+  console.log(`\nGetting information for all users.`);
   try{
     const allUsers = await User.findAll();
     res.status(200).json(allUsers);
@@ -31,6 +33,7 @@ router.get('/', async (req, res) => {
 
 // Read Single User
 router.get('/:user_id', async (req, res) => {
+  console.log(`\nGetting information for user: #${req.params.user_id}.`);
   try {
     const userData = await User.findByPk(req.params.user_id)
     res.status(200).json(userData);
@@ -41,11 +44,35 @@ router.get('/:user_id', async (req, res) => {
 });
 
 // Update Single User
-router.put('/updateUser/:user_id', async (req, res) => {
+router.put('/update/:user_id', async (req, res) => {
+  console.log(`\nUpdating information for user: #${req.params.user_id}.`);
   try{
+    const updateData = await User.update({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    },{
+      where: { id: req.params.user_id }
+    });
+    res.status(200).json(updateData);
   }catch(err){
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+router.delete('/delete/:user_id', (req, res) => {
+  console.log(`Delete user with id: ${req.params.user_id}.`);
+  try{
+    const deleteData = User.destroy({
+      where: { id: req.params.user_id }
+    });
+    res.status(200).json(deleteData);
+    }catch(err){
+      console.log(err);
+      res.status(500).json(err);
   }
 });
 
